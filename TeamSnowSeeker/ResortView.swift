f5d4658d25a694c96ce1016b4cb8c034c44204f9
Joshua Buhler <jbuhler@control4.com>
2020-11-18 15:57:17 -0700

    Adding iPad layouts

--- TeamSnowSeeker/ResortView.swift
+++ TeamSnowSeeker/ResortView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct ResortView: View {
+    
+    @Environment(\.horizontalSizeClass) var sizeClass
+    
     let resort: Resort
     
     var body: some View {
@@ -18,18 +21,24 @@ struct ResortView: View {
                     .scaledToFit()
                 
                 HStack {
-                    Spacer()
-                    ResortDetailsView(resort: resort)
-                    SkiDetailsView(resort: resort)
-                    Spacer()
+                    if sizeClass == .compact {
+                        Spacer()
+                        VStack { ResortDetailsView(resort: resort) }
+                        VStack { SkiDetailsView(resort: resort) }
+                        Spacer()
+                    } else {
+                        ResortDetailsView(resort: resort)
+                        Spacer().frame(height: 0)
+                        SkiDetailsView(resort: resort)
+                    }
                 }
                 .font(.headline)
                 .foregroundColor(.secondary)
-                .padding(.top)
+                .padding()
                 
                 Group {
                     Text(resort.description)
-                        .padding(.vertical)
+                        .padding(.bottom)
                     
                     Text("Facilities")
                         .font(.headline)
