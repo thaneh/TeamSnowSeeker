243004e6816a4cea2bab55d4322a2fd958bb75ac
Joshua Buhler <jbuhler@control4.com>
2020-11-18 15:23:13 -0700

    Moar changes

--- TeamSnowSeeker/ContentView.swift
+++ TeamSnowSeeker/ContentView.swift
@@ -8,9 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    let resorts:[Resort] = Resort.allResorts
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(resorts) { resort in
+                NavigationLink(destination: Text(resort.name)) {
+                    Image(resort.country)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 40, height: 25)
+                        .clipBorder(RoundedRectangle(cornerRadius: 5), lineWidth: 1)
+
+                    VStack(alignment: .leading) {
+                        Text(resort.name)
+                            .font(.headline)
+                        Text("\(resort.runs) runs")
+                            .foregroundColor(.secondary)
+                    }
+                }
+            }
+            .navigationBarTitle("Resorts")
+        }
     }
 }
 
