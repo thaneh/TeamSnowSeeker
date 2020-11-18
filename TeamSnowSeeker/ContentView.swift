319181274ff257c4d6be2ed7fc24f4e631a9f97b
Thane Heninger <tsheninger@control4.com>
2020-11-18 13:53:42 -0700

    Add empty project.

---
+++ TeamSnowSeeker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TeamSnowSeeker
+//
+//  Created by Thane Heninger on 11/18/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TeamSnowSeeker/TeamSnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamSnowSeekerApp.swift
+//  TeamSnowSeeker
+//
+//  Created by Thane Heninger on 11/18/20.
+//
+
+import SwiftUI
+
+@main
+struct TeamSnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
